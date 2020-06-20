511765953b896854fd37749ebe7674115f2bfedd
Oleksandr Detilie <al.detillier@gmail.com>
2020-06-21 01:14:34 +0300

    basic-effect: Implement basic Effect class
    1. Implement basic Effect class
    2. Update StealEffect
    3. Update class diagramm
    4. Update utils module

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
-from citizens.citizen import Citizen
-
 from typing import List
 
-
-def is_citizen_in_range(target_number: int, citizens: List[Citizen]) -> bool:
-    return True
+from citizens.citizen import Citizen
 
 
-def validate_citizen_target_number(target_number: int, citizens: List[Citizen]) -> bool:
-    return target_number is not None and is_citizen_in_range(target_number, citizens) and citizens[target_number].is_alive
+def is_citizen_in_range(target_index: int, citizens: List[Citizen]) -> bool:
+    return target_index >= 0 and target_index < len(citizens)
+
+
+def validate_citizen_target_number(target_number: int,
+                                   citizens: List[Citizen]) -> bool:
+    return (target_number is not None and
+            is_citizen_in_range(target_number - 1, citizens) and
+            citizens[target_number - 1].is_alive)
