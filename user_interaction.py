48c2a7fc0287bfeface6ad726b50f54506c0c887
asternotus <asternotus@gmail.com>
2020-07-03 00:08:30 +0300

    user-interaction: user_interaction.py implementation
    1. Add all print methods of user_interaction.py
    2. Add read_index method of user_interaction.py

--- user_interaction.py
+++ user_interaction.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 # TODO: implement user_interaction as a class. And pass instance of this class\
 # to every effect as argument
@@ -10,26 +11,50 @@ class MessageScope(Enum):
     PASSIVE = 3
 
 
+prepared_messages = {MessageScope.GLOBAL: [],
+                     MessageScope.ACTIVE: [], MessageScope.PASSIVE: []}
+
+
 def save_global(text: str) -> None:
-    pass
+    prepared_messages[MessageScope.GLOBAL].append(text)
 
 
 def save_active(text: str) -> None:
-    pass
+    prepared_messages[MessageScope.ACTIVE].append(text)
 
 
 def save_passive(text: str) -> None:
-    pass
+    prepared_messages[MessageScope.PASSIVE].append(text)
+
+
+def show_global_instant(text: str) -> None:
+    print(f"{MessageScope.GLOBAL.name}: {text}")
 
 
 def show_active_instant(text: str) -> None:
-    print(text)
+    print(f"{MessageScope.ACTIVE.name}: {text}")
 
 
-def read_number(text: str) -> int:
+def show_passive_instant(text: str) -> None:
+    print(f"{MessageScope.PASSIVE.name}: {text}")
+
+
+def show_all() -> None:
+    for scope, text_list in prepared_messages.items():
+        print(f"{scope.name}: {text_list}")
+    prepared_messages.clear()
+
+
+def read_index(text: str) -> Optional[int]:
     """Reading and validating inputs.
 
     If input value is valid - return int
-    If input value is NOT valie - return None
+    If input value is NOT valid - return None
     """
-    return 0
+    index = input(text)
+    if index.isnumeric():
+        index = int(text)
+    else:
+        index = None
+
+    return index
