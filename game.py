657e5d78743c6acb110b2fb2d77fcf05e74bcc23
SurprisedBird <irinayavors@gmail.com>
2020-07-12 15:16:47 +0300

    fix: Code refinement.
    1. Removing unnecessary imports.
    2. Small code fixes.

--- game.py
+++ game.py
@@ -1,20 +1,29 @@
 from typing import List
 
-from citizens.citizen import Citizen
-from citizens.player import Player
-from citizens.spy import Spy
-
 
 class Game:
     def __init__(self) -> None:
-        self.citizens: List[Citizen] = []
-        self.player: List[Player] = []
-        self.spy: Spy = None
-        self.first_player: Player = None
+        self.citizens: List['Citizen'] = []
+        self.players: List['Player'] = []
+        self.spy: 'Spy' = None
         self.round_number = 0
 
-    def get_active_player(self) -> Player:
-        pass
+    @property
+    def get_active_player(self) -> 'Player':
+        active_player = None
+        if self.round_number % 2 != 0:
+            active_player = self.players[0]
+        else:
+            active_player = self.players[1]
 
-    def get_passive_player(self) -> Player:
-        pass
+        return active_player
+
+    @property
+    def get_passive_player(self) -> 'Player':
+        passive_player = None
+        if self.round_number % 2 == 0:
+            passive_player = self.players[0]
+        else:
+            passive_player = self.players[1]
+
+        return passive_player
--- game_controller.py
+++ game_controller.py
@@ -1,4 +1,5 @@
 import random
+from typing import List
 
 import user_interaction as user_inter
 from citizens.citizen import Citizen
@@ -8,74 +9,58 @@ from game import Game
 
 
 class GameController:
-    def __init__(self) -> None:
+    def __init__(self, citizens_dict, user_names) -> None:
 
-        self.citizens_dict = {
-            "Охотник": "Ловушка",
-            "Врач": "Антидот",
-            "Сутенер": "Клофелинщица"
-        }
+        self.citizens_dict = citizens_dict
+        self.user_names = user_names
 
-        self.first_user = 'GvinP'
-        self.second_user = 'Runmaget'
-        self.available_citizens = []
+        self.game: 'Game'
 
     def start_game(self):
-        self.game = Game()
+        pass
 
     def prepare_game(self):
-        self.create_citizens()
-        self.set_order()
-        self.create_player()
-        self.create_spy()
-        self.show_game_state()
+        self.game = Game()
+        avilable_citizens: List['Citizen'] = []
 
-    def create_citizens(self):
+        self._create_citizens(avilable_citizens)
+        self._create_players(avilable_citizens)
+        self._set_order()
+        self._create_spy(avilable_citizens)
+        self._show_game_state()
+
+    def _create_citizens(self, avilable_citizens):
         for name, card in self.citizens_dict.items():
             self.game.citizens.append(Citizen(name=name, citizen_card=card))
-            self.available_citizens.append(
-                Citizen(name=name, citizen_card=card))
+            avilable_citizens.extend(self.game.citizens)
+        user_inter.show_global_instant("Loading")
 
-    def set_order(self):
-        self.user_names = [self.first_user, self.second_user]
-        random.shuffle(self.user_names)
+    def _create_players(self, avilable_citizens):
+        # TODO: first player should not have oportunity to steal
 
-    def create_player(self):
-        first_player_name = self.first_user[0]
-        second_player_name = self.second_user[1]
+        for user_name in self.user_names:
+            random_index = random.randint(0, len(avilable_citizens) - 1)
+            random_citizen = avilable_citizens.pop(random_index)
 
-        random_citizen = self.available_citizens.pop(
-            random.randint(0,
-                           len(self.available_citizens) - 1))
+            player = Player(user_name=user_name,
+                            name=random_citizen.name,
+                            citizen_card=random_citizen.citizen_card)
 
-        first_player = Player(user_name=first_player_name,
-                              name=random_citizen.name,
-                              citizen_card=random_citizen._citizen_card)
+            self.game.players.append(player)
 
-        random_citizen = self.available_citizens.pop(
-            random.randint(0,
-                           len(self.available_citizens) - 1))
+    def _set_order(self):
+        random.shuffle(self.game.players)
 
-        second_player = Player(user_name=second_player_name,
-                               name=random_citizen.name,
-                               citizen_card=random_citizen._citizen_card)
+        user_inter.save_active("You are first player")
+        user_inter.save_active("Your role is role name")
+        user_inter.save_passive("Your role is role name")
+        user_inter.save_global("First player is")
 
-        self.game.players.append(first_player)
-        self.game.first_player = first_player
-        self.game.players.append(second_player)
-
-    def create_spy(self):
-        print(self.available_citizens)
-        random_citizen = random.choice(self.available_citizens)
+    def _create_spy(self, avilable_citizens):
+        random_index = random.randint(0, len(avilable_citizens) - 1)
+        random_citizen = avilable_citizens.pop(random_index)
 
         self.game.spy = Spy(name=random_citizen.name)
 
-    def show_game_state(self):
-        pass
-
-
-if __name__ == "__main__":
-    print("GameController")
-    gc = GameController()
-    gc.start_game()
-    gc.prepare_game()
+    def _show_game_state(self):
+        user_inter.save_global("Game started")
