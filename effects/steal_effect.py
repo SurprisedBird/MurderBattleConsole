b636a87fc639b6472306af854790022e571d5600
Oleksandr Detilie <al.detillier@gmail.com>
2020-08-23 00:36:17 +0300

    fix: fix message to a private player. Code re-factoring

--- effects/steal_effect.py
+++ effects/steal_effect.py
@@ -1,3 +1,4 @@
+import message_text_config as msg
 import user_interaction
 import utils
 from citizens.citizen import Citizen
@@ -8,33 +9,41 @@ from game import Game
 
 class StealEffect(Effect):
     def __init__(self, game: Game, name: str, creator: Citizen) -> None:
-        Effect.__init__(self, game, name, creator, 0)
+        super().__init__(game, name, creator, 0)
 
     def _activate_impl(self) -> bool:
-        target_number = user_interaction.read_index("config.type_target")
+        target_number = user_interaction.read_number(
+            msg.NightActionTarget.ACT_STEAL)
         while (not self._validate(target_number)):
-            user_interaction.show_active_instant("config.type_error")
-            target_number = user_interaction.read_index("config.type_target")
+            user_interaction.show_active_instant(msg.Errors.TARGET)
+            target_number = user_interaction.read_number(
+                msg.NightActionTarget.ACT_STEAL)
 
         self._targets.append(self._game.citizens[target_number - 1])
 
         return True
 
     def _resolve_impl(self) -> bool:
-        if self._targets[0].citizen_card is not None:
-            self._creator.stolen_cards.append(self._targets[0].citizen_card)
+        citizen_card = self._targets[0].citizen_card
+        if citizen_card is not None:
+            self._creator.stolen_cards.append(citizen_card)
             self._targets[0].citizen_card = None
-            user_interaction.save_active("config.card_stolen")
-            user_interaction.save_passive("config.lost_card")
+            user_interaction.save_active(
+                msg.NightResult.ACT_STEAL_SUCCESSFULL.format(
+                    citizen_card.name))
 
+            if self._targets[0] is self._game.passive_player:
+                user_interaction.save_passive(msg.NightResult.PASS_LOST_CARD)
         else:
-            user_interaction.save_active("config.no_card")
+            user_interaction.save_active(
+                msg.NightResult.ACT_STEAL_UNSUCCESSFULL.format(
+                    self._targets[0].name))
 
         return True
 
-    def _validate(self, target_number: int):
+    def _validate(self, target_number: int) -> bool:
         return utils.validate_citizen_target_number(target_number,
                                                     self._game.citizens)
 
-    def on_clear(self) -> None:
+    def _on_clear_impl(self) -> None:
         pass
