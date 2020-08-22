7066a8b9ff38da934efa745ce932e8b8a8b77216
Oleksandr Detilie <al.detillier@gmail.com>
2020-08-23 00:44:19 +0300

    fix: updated main class

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from enum import Enum, auto
+
+from card import Card
+from effects.none_effect import NoneEffect
 from game_controller import GameController
 
 citizens_dict = {
-    "Охотник": "Ловушка",
-    "Врач": "Антидот",
-    "Сутенер": "Клофелинщица"
+    "Охотник": Card("Ловушка", NoneEffect),
+    "Врач": Card("Антидот", NoneEffect),
+    "Сутенер": Card("Клофелинщица", NoneEffect)
 }
 
 user_names = ["GvinP", "Runmaget"]
@@ -11,5 +15,4 @@ user_names = ["GvinP", "Runmaget"]
 if __name__ == "__main__":
     gc = GameController(citizens_dict, user_names)
 
-    gc.prepare_game()
     gc.start_game()
