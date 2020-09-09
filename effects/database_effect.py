7cb57bce78255a6f308ca4fe3352ba4e9f77ab4e
SurprisedBird <irinayavors@gmail.com>
2020-09-09 10:53:16 +0300

    feat: implements database effect methods - add errors messages in message_text_config - implement database effect methods

---
+++ effects/database_effect.py
@@ -0,0 +1,87 @@
+from enum import Enum
+from typing import List
+
+import message_text_config as config
+import user_interaction
+import utils
+from citizens.citizen import Citizen
+from effects.effect import Effect
+from game import Game
+from message_text_config import Errors
+
+
+class ErrorsEnum(Enum):
+    INVALID_TURGET = Errors.TARGET
+    NOT_THREE_TARGET = Errors.DATA_BASE_TARGETS
+    SAME_TARGETS = Errors.DATA_BASE_SAME_TARGETS
+
+
+class DataBase(Effect):
+    def __init__(self, game: Game, name: str, creator: Citizen) -> None:
+        super().__init__(game, name, creator, 4)
+        self._errors = None
+
+    def _activate_impl(self):
+        target_numbers = []
+        for _ in range(3):
+            target_number = user_interaction.read_number(
+                config.CardTarget.ACT_DATABASE)
+            target_numbers.append(target_number)
+
+        while (not self._validate(target_numbers)):
+            user_interaction.show_active_instant(self._status.value)
+            target_numbers = []
+            for _ in range(3):
+                target_number = user_interaction.read_number(
+                    config.CardTarget.ACT_DATABASE)
+                target_numbers.append(target_number)
+
+        self._targets = [
+            self._game.citizens[target_number - 1]
+            for target_number in target_numbers
+        ]
+        return True
+
+    def _validate(self, target_numbers: List[int]):
+
+        for target in target_numbers:
+            if not utils.validate_citizen_target_number(
+                    target, self._game.citizens):
+                return False
+        else:
+            is_in_range = True
+
+        if not is_in_range:
+            self._status = ErrorsEnum.INVALID_TURGET
+            return False
+
+        is_three_targets = (len(target_numbers) == 3)
+        if not is_three_targets:
+            self._status = ErrorsEnum.NOT_THREE_TARGET
+            return False
+
+        #check if player have chosen 3 different targets
+        is_different = (len(target_numbers) == len(set(target_numbers)))
+        if not is_different:
+            self._status = ErrorsEnum.SAME_TARGETS
+            return False
+
+        return True
+
+    def _resolve_impl(self):
+        roles = [type(target).__name__ for target in self._targets]
+        if ("Player" in roles) and ("Spy" in roles):
+            user_interaction.show_active_instant(
+                config.EffectsResolved.ACT_DATABASE_FIND_ALL)
+        elif "Player" in roles:
+            user_interaction.show_active_instant(
+                config.EffectsResolved.ACT_DATABASE_FIND_PLAYER)
+        elif "Spy" in roles:
+            user_interaction.show_active_instant(
+                config.EffectsResolved.ACT_DATABASE_FIND_SPY)
+        else:
+            user_interaction.show_active_instant(
+                config.EffectsResolved.ACT_DATABASE_NO_SUSPECT)
+
+    def _on_clear_impl(self):
+        pass
---
+++ tests/unit/database_test.py
@@ -0,0 +1,87 @@
+import random
+import string
+from typing import List
+from unittest import TestCase
+from unittest.mock import patch
+
+import message_text_config as config
+import user_interaction
+from card import Card
+from citizens.citizen import Citizen
+from effects.database_effect import DataBase
+from effects.none_effect import NoneEffect
+from game import Game
+from game_controller import GameController
+
+
+class DatabaseEffectTest(TestCase):
+    def setUp(self):
+        # seed makes similar citizens players data
+        random.seed(408245472491)
+        citizens_dict = {
+            "Охотник": Card("Ловушка", NoneEffect),
+            "Врач": Card("Антидот", NoneEffect),
+            "Мальчик-хакер": Card("База данных", NoneEffect),
+            "Актер": Card("Реквизит", NoneEffect),
+            "Сутенер": Card("Клофелинщица", NoneEffect)
+        }
+
+        user_names = ["GvinP", "Runmaget"]
+
+        self.gc = GameController(citizens_dict, user_names)
+
+        self.gc._prepare_game()
+
+        citizen = Citizen("Мальчик-хакер", "База данных", 3)
+        self.db_effect = DataBase(self.gc.game, "База данных", citizen)
+
+        self.gc._show_night_state()
+
+    def test_activate_impl(self):
+        with patch('builtins.input',
+                   side_effect=["0", "0", "0", "1", "2", "3"]):
+            self.db_effect._activate_impl()
+
+    def test_validate(self):
+        self.assertEqual(self.db_effect._validate([1, 3, 2]), True)
+        self.assertEqual(self.db_effect._validate([3441, "3dsff", "dsfasd"]),
+                         False)
+        self.assertEqual(self.db_effect._validate([]), False)
+        self.assertEqual(self.db_effect._validate([1, 3, 3]), False)
+        self.assertEqual(self.db_effect._validate([1]), False)
+        self.assertEqual(self.db_effect._validate([1, 3, 2, 4]), False)
+
+    @patch('builtins.print')
+    def test_resolve_impl(self, mock_print):
+
+        self.db_effect._targets = [
+            self.gc.game.citizens[0], self.gc.game.citizens[1]
+        ]
+
+        self.db_effect._resolve_impl()
+        mock_print.assert_called_with(
+            f"ACTIVE: {config.EffectsResolved.ACT_DATABASE_NO_SUSPECT}")
+
+        self.db_effect._targets = [
+            self.gc.game.citizens[0], self.gc.game.citizens[4]
+        ]
+
+        self.db_effect._resolve_impl()
+        mock_print.assert_called_with(
+            f"ACTIVE: {config.EffectsResolved.ACT_DATABASE_FIND_SPY}")
+
+        self.db_effect._targets = [
+            self.gc.game.citizens[3], self.gc.game.citizens[1]
+        ]
+
+        self.db_effect._resolve_impl()
+        mock_print.assert_called_with(
+            f"ACTIVE: {config.EffectsResolved.ACT_DATABASE_FIND_PLAYER}")
+
+        self.db_effect._targets = [
+            self.gc.game.citizens[3], self.gc.game.citizens[4]
+        ]
+
+        self.db_effect._resolve_impl()
+        mock_print.assert_called_with(
+            f"ACTIVE: {config.EffectsResolved.ACT_DATABASE_FIND_ALL}")
