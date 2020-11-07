57d87396bf6f42e21cb129cbecb7458ddd4ed9c5
asternotus <asternotus@gmail.com>
2020-11-07 14:50:03 +0200

    Add all citizens in the main with effects in the priority order (#24)

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from enum import Enum, auto
 
 from card import Card
+from effects.alarm_effect import AlarmEffect
+from effects.database_effect import DatabaseEffect
+from effects.gossips_effect import GossipsEffect
 from effects.none_effect import NoneEffect
+from effects.trap_effect import TrapEffect
+from effects.videocamera_effect import VideoCameraEffect
+from effects.whore_effect import WhoreEffect
 from game_controller import GameController
 
 citizens_dict = {
-    "Охотник": Card("Ловушка", NoneEffect),
+    "Мальчик Хакер": Card("База данных", DatabaseEffect),
+    "Ночная продавщица": Card("Сплетни", GossipsEffect),
+    "Начальник охраны": Card("Видеокамера", VideoCameraEffect),
+    "Сутенер": Card("Проститутка", WhoreEffect),
+    "Надзиратель": Card("Система тревоги", AlarmEffect),
+    "Охотник": Card("Ловушка", TrapEffect),
+    "Полицейский": Card("Защита свидетеля", NoneEffect),
+    "Актер": Card("Театральный реквизит", NoneEffect),
+    "Наркоман": Card("Наркотики", NoneEffect),
+    "Вечно недовольная старушка": Card("Анонимный звонок", NoneEffect),
+    "Соцработник": Card("Налоги", NoneEffect),
     "Врач": Card("Антидот", NoneEffect),
-    "Сутенер": Card("Клофелинщица", NoneEffect)
+    "Бригадир": Card("Банда", NoneEffect)
 }
 
 user_names = ["GvinP", "Runmaget"]
