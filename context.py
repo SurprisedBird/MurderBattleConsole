4758de182df0617352d49af6d1aa0e364a740dfd
asternotus <asternotus@gmail.com>
2020-12-05 15:40:11 +0200

    feat: add Context class and property methods into the game_controller.py

---
+++ context.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class Context(ABC):
+    @property
+    @abstractmethod
+    def user_interaction(self):
+        pass
+
+    @property
+    @abstractmethod
+    def game(self):
+        pass
+
+    @property
+    @abstractmethod
+    def action_history(self):
+        pass
