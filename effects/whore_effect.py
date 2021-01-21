05061a27a7c3ab68bb2be1bb2b287b3dec3a0d3f
asternotus <asternotus@gmail.com>
2021-01-21 16:50:51 +0200

    bugfix: Change whore init method param from game to context

--- effects/whore_effect.py
+++ effects/whore_effect.py
@@ -8,8 +8,8 @@ from effects.effect import Effect
 
 
 class WhoreEffect(Effect):
-    def __init__(self, game: Game, name: str, creator: Citizen) -> None:
-        super().__init__(game, name, creator, 7)
+    def __init__(self, context: 'Context', name: str, creator: Citizen) -> None:
+        super().__init__(context, name, creator, 7)
 
     def _activate_impl(self) -> bool:
         target_number = utils.read_target_number(
