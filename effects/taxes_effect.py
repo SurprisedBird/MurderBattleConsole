714751097aa91ec3ba99bb816a458bf15642a087
Oleksandr Detilie <al.detillier@gmail.com>
2021-01-29 00:01:25 +0200

    fix: change taxes logic
    Make taxes work same for citizens and players
    Updated input messages logic for taxes

--- effects/taxes_effect.py
+++ effects/taxes_effect.py
@@ -1,19 +1,18 @@
 from enum import Enum, auto
-from typing import Tuple
+from typing import List
 
 import message_text_config as msg
 import utils
 from citizens.citizen import Citizen
-from citizens.player import Player
 from citizens.spy import Spy
 from game import Game
 
-from effects.effect import Effect
+from effects.effect import Effect, InputStatusCode
 
 
 class PaymentChoice(Enum):
-    HP_CHOICE = auto()
-    CARD_CHOICE = auto()
+    HP_OPTION = auto()
+    CARD_OPTION = auto()
 
 
 class TaxesEffect(Effect):
@@ -33,73 +32,82 @@ class TaxesEffect(Effect):
         return True
 
     def _resolve_impl(self) -> bool:
-        if isinstance(self.targets[0], Player):
-            if self.activation_round == self.game.round_number:
-                return False
-            else:
-                payment_choice = self._choose_payment()
-                if payment_choice == PaymentChoice.HP_CHOICE:
-                    self.targets[0].hp -= 1
-                elif payment_choice == PaymentChoice.CARD_CHOICE and (
-                        self.targets[0].citizen_card is not None
-                        or len(self.targets[0].stolen_cards) > 0):
-                    self._choose_card_payment()
+        if self.activation_round == self.game.round_number:
+            return False
+
+        if utils.is_player(self.targets[0]):
+            payment_choice = self._choose_payment()
+            if payment_choice == PaymentChoice.HP_OPTION:
+                self.targets[0].hp -= 1
+            elif payment_choice == PaymentChoice.CARD_OPTION:
+                card = self._choose_card()
+                self._remove_card_from_target(card)
         else:
-            self.targets[0].citizen_card = None
+            if self.targets[0].citizen_card is None:
+                self.targets[0].hp -= 1
+            else:
+                self.targets[0].citizen_card = None
+
         return True
 
-    def _validate(self, target_number: int) -> bool:
-        return utils.validate_citizen_target_number(target_number,
-                                                    self.game.citizens)
+    def _validate(self, target_number: int) -> InputStatusCode:
+        return utils.validate_citizen_target_number(
+            target_number,
+            self.game.citizens,
+            self_as_target_allowed=False,
+            creator=self.creator)
 
     def _on_clear_impl(self) -> None:
         pass
 
-    def _choose_payment(self) -> bool:
-        while (True):
-            taxes_choice = self.user_interaction.read_number(
-                msg.TaxesMessages.RESOLVE_PLAYER_CHOOSE_VARIANT)
-            if taxes_choice == 1:
-                return PaymentChoice.HP_CHOICE
-            elif taxes_choice == 2 and (
-                    self.targets[0].citizen_card is not None
-                    or len(self.targets[0].stolen_cards) > 0):
-                return PaymentChoice.CARD_CHOICE
-            else:
-                self.user_interaction.show_active_instant(
-                    msg.Errors.CONFIRM_CHOICE)
+    def _create_card_list(self) -> List['Card']:
+        card_list = self.targets[0].stolen_cards[:]
 
-    def _choose_card_payment(self) -> bool:
-        while (True):
-            cards_names = ""
-            for i, card_name in enumerate(self._create_cards_names_list(),
-                                          start=1):
-                cards_names += (f"{i}. {card_name}\n")
-
-            card_choice = self.user_interaction.read_number(
-                f"\n{msg.PlayerMessages.CHOOSE_CARD_ACTION}\n{cards_names}")
-
-            index = 1
-            if self.targets[0].citizen_card is None:
-                index == 0
-
-            if card_choice == index:
-                self.targets[0].citizen_card = None
-                break
-
-            elif card_choice is not None and card_choice in range(
-                    index,
-                    len(self.targets[0].stolen_cards) + 1 + index):
-                del self.targets[0].stolen_cards[card_choice - 1 - index]
-                break
-            else:
-                self.user_interaction.show_active_instant(
-                    msg.Errors.CONFIRM_CHOICE)
-
-    def _create_cards_names_list(self) -> 'List':
-        cards_names_list = []
         if self.targets[0].citizen_card is not None:
-            cards_names_list.append(self.targets[0].citizen_card.name)
-        for card in self.targets[0].stolen_cards:
-            cards_names_list.append(card.name)
-        return cards_names_list
+            card_list.append(self.targets[0].citizen_card)
+
+        return card_list
+
+    def _choose_payment(self) -> PaymentChoice:
+        target_has_cards = self.targets[0].citizen_card is not None or len(
+            self.targets[0].stolen_cards) > 0
+
+        payment_options = f"{msg.TaxesMessages.RESOLVE_PLAYER_CHOOSE_VARIANT}\n"
+        payment_options += f"{msg.TaxesMessages.RESOLVE_PLAYER_HP_OPTION}\n"
+        if target_has_cards:
+            payment_options += f"{msg.TaxesMessages.RESOLVE_PLAYER_CARD_OPTION}\n"
+
+        while (True):
+            payment_choice = self.user_interaction.read_number(payment_options)
+
+            if payment_choice == 1:
+                return PaymentChoice.HP_OPTION
+            elif payment_choice == 2 and target_has_cards:
+                return PaymentChoice.CARD_OPTION
+            else:
+                self.user_interaction.show_active_instant(
+                    msg.CommonMessages.ERROR_INVALID_OPTION)
+
+    def _choose_card(self) -> 'Card':
+        card_list = self._create_card_list()
+
+        card_options = f"{msg.PlayerMessages.CHOOSE_CARD_ACTION}\n"
+        for i, card in enumerate(card_list, start=1):
+            card_options += (f"{i}. {card.name}\n")
+
+        while (True):
+            card_choice = self.user_interaction.read_number(card_options)
+
+            if card_choice is None or \
+                1 > card_choice > len(self.targets[0].stolen_cards):
+                self.user_interaction.show_active_instant(
+                    msg.CommonMessages.ERROR_INVALID_OPTION)
+                continue
+
+            return card_list[card_choice - 1]
+
+    def _remove_card_from_target(self, card: 'Card') -> None:
+        if self.targets[0].citizen_card is card:
+            self.targets[0].citizen_card = None
+        else:
+            self.targets[0].stolen_cards.remove(card)
