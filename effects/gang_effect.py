defe70aa3132a032fe1553c9d0e2af2c24f5695d
SurprisedBird <irinayavors@gmail.com>
2021-02-20 14:37:02 +0200

    feat: add gang effect:
    - chose the creator as a target

---
+++ effects/gang_effect.py
@@ -0,0 +1,52 @@
+import message_text_config as msg
+import utils
+from citizens.citizen import Citizen
+from citizens.player import Player
+from game import Game
+
+from effects.effect import Effect, EffectStatus, InputStatusCode
+
+
+class GangEffect(Effect):
+    def __init__(self, context: 'Context', name: str,
+                 creator: Citizen) -> None:
+        super().__init__(context, name, creator, 13)
+
+    def _activate_impl(self) -> bool:
+        self.targets.append(self.creator)
+        self.user_interaction.save_active(msg.GangMessages.ACTIVATION_SUCCESS)
+
+        return True
+
+    def _resolve_impl(self) -> bool:
+        if self.game.round_number == self.activation_round:
+            return False
+
+        card_effects_list = []
+        citizens = self.context.game.citizens
+
+        for citizen in citizens:
+            card_effect = (effect for effect in citizen.effects
+                           if self.is_valid_card_effect(effect))
+
+            for effect in card_effect:
+                card_effects_list.append(effect)
+
+        for effect in card_effects_list:
+            effect.deactivate()
+            self.user_interaction.save_active(msg.GangMessages.RESOLVE_SUCCESS)
+
+        return True
+
+    def is_valid_card_effect(self, effect: 'Effect') -> bool:
+        is_current_round = effect.activation_round == self.game.round_number
+        not_an_action = (not utils.is_action_effect(effect))
+
+        return is_current_round and not_an_action
+
+    def _validate(self, target_number: int) -> InputStatusCode:
+        return utils.validate_citizen_target_number(target_number,
+                                                    self.game.citizens)
+
+    def _on_clear_impl(self) -> None:
+        pass
