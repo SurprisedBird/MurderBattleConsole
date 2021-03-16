803d76960f0323b4ab4750dc4f112a15b5866e60
asternotus <asternotus@gmail.com>
2021-03-16 12:50:31 +0200

    feat: First night effect fixed

---
+++ effects/first_night_effect.py
@@ -0,0 +1,30 @@
+from typing import Tuple
+
+import message_text_config as msg
+import utils
+from citizens.citizen import Citizen
+from game import Game
+
+from effects.effect import Effect, InputStatusCode
+
+class FirstNightEffect(Effect):
+    def __init__(self, context: 'Context', name: str,
+                 creator: Citizen) -> None:
+        super().__init__(context, name, creator, 20)
+
+    def _activate_impl(self) -> bool:
+        self.targets.append(self.game.passive_player)
+        return True
+
+    def _resolve_impl(self) -> bool:
+        if self.activation_round == self.game.round_number:
+            self.targets[0].disable_steal_action()
+            return False
+
+        return True
+
+    def _validate(self, target_number: int) -> InputStatusCode:
+        return InputStatusCode.OK
+
+    def _on_clear_impl(self) -> None:
+        self.targets[0].enable_steal_action()
